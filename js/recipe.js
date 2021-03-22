3db8aae3598def3e4d205e17b3ce3dc10d1bc454
Odysseus7 <chalitandunu130@gmail.com>
2021-03-22 21:26:14 +0100

    Finish recipe page

--- js/recipe.js
+++ js/recipe.js
@@ -1,56 +1,73 @@
-import {handleError} from './results.js';
+import { handleError } from "./results.js";
 
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("drinkId");
+const keyword = urlParams.get("keyword");
+const filter = urlParams.get("filter");
+
 const container = document.querySelector(".container");
 
 const fetchRecipe = async (id) => {
+	await axios
+		.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+		.then((response) => {
+			const data = response.data.drinks[0];
+			if (data != null) {
+				handleDisplay(data);
+			} else {
+				handleError();
+			}
+		});
+};
 
-    await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
-    .then(response => {
-        const data = response.data.drinks[0];
-        if(data != null) {
-            handleDisplay(data);
-        } else {
-            handleError();
-        }
-    });
-    
-}
+const handleDisplay = (cocktail) => {
+	document.title = cocktail.strDrink;
+	container.insertAdjacentHTML(
+		"beforebegin",
+		`
+        <div class="back">
+            <a href="./results.html?keyword=${keyword}&filter=${filter}"><h1>&larr; Go back</h1></a>
+        </div>
+    `
+	);
 
-const handleDisplay = cocktail => {
-    console.log(cocktail);
-    document.title = cocktail.strDrink;
+	//Check which ingredients are used, get keys and save corresponding values
+	const ingredientKeys = Object.keys(cocktail).filter(
+		(key) => key.includes("strIngredient") && cocktail[key]
+	);
+	const measureKeys = ingredientKeys.map((key) =>
+		key.replace("Ingredient", "Measure")
+	);
 
-    //Check which ingredients are used, get keys and save corresponding values
-    const ingredientKeys = Object.keys(cocktail).filter(key => key.includes("strIngredient") && cocktail[key] != null);
-    const measureKeys = ingredientKeys.map(key => key.replace("Ingredient", "Measure"));
-    const measures = measureKeys.map(key => cocktail[key].trim());
-    const ingredients = ingredientKeys.map(key => cocktail[key]);
-    
-    // concatenate measure to ingredient
-    
-    const completeIngredients = measures.map((measure, index) => {
-        return measure + " " + ingredients[index];
-    });
-   
-    container.insertAdjacentHTML('afterbegin', `
+	const measures = measureKeys.map((key) => cocktail[key].trim());
+	const ingredients = ingredientKeys.map((key) => cocktail[key]);
+
+	// concatenate measure to ingredient
+
+	const completeIngredients = measures.map((measure, index) => {
+		return measure + " " + ingredients[index];
+	});
+
+	container.insertAdjacentHTML(
+		"afterbegin",
+		`
         <article class="card" data-id=${cocktail.idDrink}>
             <figure>
                 <img src="${cocktail.strDrinkThumb}" alt=${cocktail.strDrink}>
             </figure>
             <h2>${cocktail.strDrink}</h2>
             <h3>Ingredients</h3>
-            <ul class="ingredients"></ul>
+            <ol class="ingredients"></ol>
             <p>${cocktail.strInstructions}</p>
         </article> 
-    `);
+    `
+	);
 
-    completeIngredients.forEach(ingredient => {
-        document.querySelector(".ingredients").insertAdjacentHTML('afterbegin',
-    `<li>${ingredient}</li>`);
-    })
-    
-}
+	completeIngredients.forEach((ingredient) => {
+		document
+			.querySelector(".ingredients")
+			.insertAdjacentHTML("beforeend", `<li>${ingredient}</li>`);
+	});
+};
 
 window.onload = fetchRecipe(id);
--- js/results.js
+++ js/results.js
@@ -5,44 +5,48 @@ const filter = urlParams.get("filter");
 const container = document.querySelector(".container");
 
 const fetchRecipesByName = async (name) => {
-    await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`)
-    .then(response => {
-        const data = response.data.drinks;
-        if(data != null) {
-            document.title = name;
-            handleDisplay(data);
-            
-        } else {
-            handleError();
-        }
-    });
-
-    
-}
+	await axios
+		.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`)
+		.then((response) => {
+			const data = response.data.drinks;
+			if (data != null) {
+				document.title = name;
+				handleDisplay(data);
+			} else {
+				handleError();
+			}
+		});
+};
 
 const fetchRecipesByIngredient = async (ingredient) => {
-    await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`)
-    .then(response => {
-        const data = response.data.drinks;
-        if(data) {
-            handleDisplay(data);
-        } else {
-            handleError();
-        }
-    });
-}
+	await axios
+		.get(
+			`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`
+		)
+		.then((response) => {
+			const data = response.data.drinks;
+			if (data) {
+				handleDisplay(data);
+			} else {
+				handleError();
+			}
+		});
+};
 
 const handleDisplay = (cocktails) => {
-    container.insertAdjacentHTML('beforebegin', `
+	container.insertAdjacentHTML(
+		"beforebegin",
+		`
         <div class="back">
             <a href="./index.html"><h1>&larr; Go back</h1></a>
         </div>
-    `);
+    `
+	);
 
-    cocktails.forEach(cocktail => {
-        container.insertAdjacentHTML(
-        'beforeend',
-        `<article class="card" data-id=${cocktail.idDrink}>
+	cocktails.forEach((cocktail) => {
+		container.insertAdjacentHTML(
+			"beforeend",
+			`<article class="card" data-id=${cocktail.idDrink}>
             <figure>
                 <img src="${cocktail.strDrinkThumb}" alt=${cocktail.strDrink}>
             </figure>
@@ -51,47 +55,47 @@ const handleDisplay = (cocktails) => {
             <button class="btn">Get instructions</button>
         </article>
         `
-        );
-        
-    });
+		);
+	});
 
-    document.querySelectorAll(".btn").forEach(button => {
-            button.addEventListener("click", () => {
-                window.location.href = `./../recipe.html?drinkId=${button.parentNode.dataset.id}`;
-            });
-    });
-
-    
-
-
-}
+	document.querySelectorAll(".btn").forEach((button) => {
+		button.addEventListener("click", () => {
+			window.location.href = `./../recipe.html?drinkId=${button.parentNode.dataset.id}&keyword=${keyword}&filter=${filter}`;
+		});
+	});
+};
 
 export const handleError = () => {
-    container.insertAdjacentHTML('beforebegin', `
+	container.insertAdjacentHTML(
+		"beforebegin",
+		`
         <div class="back">
             <a href="./index.html"><h1>&larr; Go back</h1><a href="./index.html">
         </div>
-    `);
+    `
+	);
 
-    document.querySelector(".container").insertAdjacentHTML(
-        'afterbegin',
-        `<div class="heading">
+	document.querySelector(".container").insertAdjacentHTML(
+		"afterbegin",
+		`<div class="heading">
             <h1 class="errorHeading">404: No cocktails were found</h1>
         </div>
         `
-        );
-}
+	);
+};
 
 window.onload = () => {
-    // check if url parameters change
-    if(filter != "name" && filter != "ingredient" && window.location.href == "./results.html") {
-        alert("invalid filter");
-        window.location.href = "./index.html";
-    } else if(filter == "name") {
-        fetchRecipesByName(keyword)
-    } else if(filter == "ingredient") {
-        fetchRecipesByIngredient(keyword);
-    }
-}
-
-
+	// check if url parameters change
+	if (
+		filter != "name" &&
+		filter != "ingredient" &&
+		window.location.href == "./results.html"
+	) {
+		alert("invalid filter");
+		window.location.href = "./index.html";
+	} else if (filter == "name") {
+		fetchRecipesByName(keyword);
+	} else if (filter == "ingredient") {
+		fetchRecipesByIngredient(keyword);
+	}
+};
