bc422a0a7482044c9b7c8dc5aa1570a7a9082585
Odysseus7 <chalitandunu130@gmail.com>
2021-03-22 20:34:45 +0100

    Get ingredient list working with JS

--- js/recipe.js
+++ js/recipe.js
@@ -21,15 +21,36 @@ const fetchRecipe = async (id) => {
 const handleDisplay = cocktail => {
     console.log(cocktail);
     document.title = cocktail.strDrink;
+
+    //Check which ingredients are used, get keys and save corresponding values
+    const ingredientKeys = Object.keys(cocktail).filter(key => key.includes("strIngredient") && cocktail[key] != null);
+    const measureKeys = ingredientKeys.map(key => key.replace("Ingredient", "Measure"));
+    const measures = measureKeys.map(key => cocktail[key].trim());
+    const ingredients = ingredientKeys.map(key => cocktail[key]);
+    
+    // concatenate measure to ingredient
+    
+    const completeIngredients = measures.map((measure, index) => {
+        return measure + " " + ingredients[index];
+    });
+   
     container.insertAdjacentHTML('afterbegin', `
         <article class="card" data-id=${cocktail.idDrink}>
             <figure>
                 <img src="${cocktail.strDrinkThumb}" alt=${cocktail.strDrink}>
             </figure>
             <h2>${cocktail.strDrink}</h2>
+            <h3>Ingredients</h3>
+            <ul class="ingredients"></ul>
             <p>${cocktail.strInstructions}</p>
         </article> 
     `);
+
+    completeIngredients.forEach(ingredient => {
+        document.querySelector(".ingredients").insertAdjacentHTML('afterbegin',
+    `<li>${ingredient}</li>`);
+    })
+    
 }
 
 window.onload = fetchRecipe(id);
