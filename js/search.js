4c275dcd203f4e513935ad66626dde1079057a91
Odysseus7 <chalitandunu130@gmail.com>
2021-03-28 20:18:42 +0200

    remove error

--- js/search.js
+++ js/search.js
@@ -1,21 +1,22 @@
 const searchBar = document.querySelector(".searchbar");
 
-
 const handleSearch = (event) => {
-    var letters = /^[a-zA-Z\s]*$/;
-    let keyword = event.target.value.toLowerCase();
-    const filter = document.querySelector("input[type=radio]:checked").value;
+	if (event.key === "Enter") {
+		var letters = /^[a-zA-Z\s]*$/;
 
-    if(keyword.match(letters)) {
-        // if the input is valid, redirect and save search term and filter used.
-        window.location.href = `results.html?keyword=${event.target.value}&filter=${filter}`;
-    } else {
-        alert("invalid input");
-    }
+		let keyword = event.target.value.toLowerCase();
+		const filter = document.querySelector("input[type=radio]:checked").value;
 
-}
-
-window.onload = () => {
-    searchBar.addEventListener("change", handleSearch);
+		if (keyword.match(letters)) {
+			// if the input is valid, redirect and save search term and filter used.
+			window.location.href = `results.html?keyword=${event.target.value}&filter=${filter}`;
+			searchBar.value = "";
+		} else {
+			alert("invalid input");
+		}
+	}
 };
 
+window.onload = () => {
+	searchBar.addEventListener("keypress", handleSearch);
+};
