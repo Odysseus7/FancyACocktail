4fd810fff4371b0f855342856c8bbd1170aafa23
Odysseus7 <chalitandunu130@gmail.com>
2021-04-24 00:12:18 +0200

    Change search input type, replace article with form and add fieldset and legend

--- js/search.js
+++ js/search.js
@@ -2,12 +2,15 @@ const searchBar = document.querySelector(".searchbar");
 
 const handleSearch = (event) => {
 	if (event.key === "Enter") {
+		event.preventDefault();
 		var letters = /^[a-zA-Z\s]*$/;
 
 		let keyword = event.target.value.toLowerCase();
+		console.log(keyword);
 		const filter = document.querySelector("input[type=radio]:checked").value;
 
 		if (keyword.match(letters)) {
+			console.log("heyy");
 			// if the input is valid, redirect and save search term and filter used.
 			window.location.href = `results.html?keyword=${event.target.value}&filter=${filter}`;
 			searchBar.value = "";
